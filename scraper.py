47969970f60032bf13d302083e375dc57874e9c5
Raj Gupta <raazgupta@gmail.com>
2011-07-10 12:22:12 +0800

    now getting title and 7 word body

--- scraper.py
+++ scraper.py
@@ -1,25 +1,45 @@
+#import the necessary libraries
 import urllib2
 import re
 from BeautifulSoup import BeautifulSoup
 
-urlStr = 'http://www.kalzumeus.com/2011/07/08/business-psychology/'
+#Enter the website you want to parse. Not doing any error checking
+#at this point
+urlStr  = raw_input("Enter the website url:")
 
+#Opening site content and converting to BeautifulSoup format
 page = urllib2.urlopen(urlStr)
-
 soup = BeautifulSoup(''.join(page))
 
-letterRegEx = re.compile(r'\w+')
+#first just trying to find the title in the header tag and 
+#printing nothing if no title found
+try:
+	titleTag = soup.html.title
+	print titleTag.string
+except:
+	noTitleError = True
+	
+#Now the tough part
+#Create a regular expressions that searchs for a string of 7 words
+wordsRegEx = re.compile(r'\w+\s\w+\s\w+\s\w+\s\w+\s\w+\s\w+')
 
 #find the body of the html 
-body = soup.html.body
+try:
+	body = soup.html.body
 
-senList = soup.findAll(text=letterRegEx)
+	#find all sentences in body that have 7 or more words
+	senList = body.findAll(text=wordsRegEx)
 
-for sen in senList:
-	try:
-		if sen.parent.name != 'script':
-			print sen
-			if sen.parent.name == 'a':
-				print sen.parent['href']
-	except:
-		print "error encoding"
+	#print out each sentence, gracefully in paras
+	for sen in senList:
+		try:
+			#Check that the sentence isn't a part of javascript
+			if sen.parent.name != 'script':
+				print sen
+				print
+				# if sen.parent.name == 'a':
+					# print sen.parent['href']
+		except:
+			encodingError = True
+except:
+	noBodyError = True
