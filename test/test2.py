887d28809dab0ad433fa5077aa0862858dda92a5
Raj Gupta <raazgupta@gmail.com>
2011-07-12 12:19:29 +0800

    able to use python cgi to pull webpage data!!!

--- test/test2.py
+++ test/test2.py
@@ -2,6 +2,8 @@
 # -*- coding: UTF-8 -*-
 
 import cgi
+#from BeautifulSoup import BeautifulSoup
+import urllib2
 
 # enable debugging
 import cgitb
@@ -13,9 +15,16 @@ print
 def main():
 	
 	form = cgi.FieldStorage()
-	if form.has_key("firstname") and form["firstname"] != "":
-		print "<h1>Hello", form["firstname"].value, "</h1>"
+	if form.has_key("website") and form["website"] != "":
+		print form["website"].value
+		urlStr = form["website"].value
+		page = urllib2.urlopen(urlStr)
+		lines = page.readlines()
+		for line in lines:
+			print line
+		#soup = BeautifulSoup(''.join(page))
+		#print soup.prettify()
 	else:
-		print "<h1>Error! Please enter a first name</h1>"
+		print "<h1>Error! Please enter a website</h1>"
 		
 main()
